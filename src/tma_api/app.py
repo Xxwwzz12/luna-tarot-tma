05bf55d273d2624e7cb5544582151968a4c452ff
Xxwwzz12 <konezvsem@gmail.com>
2025-11-24 20:09:05 +0500

    Fix src problems

--- src/tma_api/app.py
+++ src/tma_api/app.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 from fastapi.exceptions import RequestValidationError
 from starlette.exceptions import HTTPException as StarletteHTTPException
+from src.config import get_settings
 
 from fastapi.middleware.cors import CORSMiddleware
 
